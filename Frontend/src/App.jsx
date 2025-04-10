ab990d15a07fdec7881488682615a6918696b2c3
Mufaddal <mufaddaltopiwala15@gmail.com>
2025-04-10 11:09:57 +0530

    frontend init

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">Hello, Tailwind with Vite!</h1>
+    </div>
+  );
+}
